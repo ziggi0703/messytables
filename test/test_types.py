8bcb8c0c97bb91ec120e02b483845dd2eb883255
Michael Ziegler <michael(dot)ziegler(at)blue-yonder.com>
2014-09-25 17:38:04 +0200

    Test DateType initialization

---
+++ test/test_types.py
@@ -0,0 +1,10 @@
+# -*- coding: <encoding name> -*-
+from unittest import TestCase
+
+from nose.tools import assert_raises
+from messytables import DateType
+
+
+class TestDateType(TestCase):
+    def test_initialization(self):
+        assert_raises(ValueError, DateType, 'Not-a-valid-date-format')
